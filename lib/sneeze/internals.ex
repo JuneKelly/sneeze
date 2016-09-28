c3729b9a07ec5f922d36a86258970a7c3fe57f02
Shane Kilkelly <shane@kilkelly.me>
2016-09-28 17:59:55 +0100

    Tags, etc

--- lib/sneeze/internals.ex
+++ lib/sneeze/internals.ex
@@ -5,8 +5,26 @@ defmodule Sneeze.Internals do
     |> Enum.join(" ")
   end
 
-  def render_tag(data) do
-    ""
+  def render_opening_tag(tag_name) do
+    "<#{tag_name}>"
   end
+  def render_opening_tag(tag_name, attribs) do
+    attrib_string = attributes_to_string(attribs)
+    "<#{tag_name} #{attrib_string}>"
+  end
+
+  def render_closing_tag(tag_name) do
+    "</#{tag_name}>"
+  end
+
+  def render_self_closing_tag(tag_name) do
+    "<#{tag_name} />"
+  end
+
+  def render_self_closing_tag(tag_name, attribs) do
+    attrib_string = attributes_to_string(attribs)
+    "<#{tag_name} #{attrib_string} />"
+  end
+
 
 end
--- test/sneeze_test.exs
+++ test/sneeze_test.exs
@@ -19,8 +19,30 @@ defmodule SneezeInternalsTest do
   use ExUnit.Case
   doctest Sneeze.Internals
 
+  alias Sneeze.Internals
+
   test "create attribute string from map" do
     attribs = %{class: "foo", id: "bar"}
-    assert Sneeze.Internals.attributes_to_string(attribs) == "class=\"foo\" id=\"bar\""
+    assert Internals.attributes_to_string(attribs) == "class=\"foo\" id=\"bar\""
   end
+
+  test "empty attributes" do
+    attribs = %{}
+    assert Internals.attributes_to_string(attribs) == ""
+  end
+
+  test "opening tag" do
+    assert Internals.render_opening_tag(:p) == "<p>"
+    assert Internals.render_opening_tag(:p, %{class: "greeting"}) == "<p class=\"greeting\">"
+  end
+
+  test "closing tag" do
+    assert Internals.render_closing_tag(:p) == "</p>"
+  end
+
+  test "self-closing tag" do
+    assert Internals.render_self_closing_tag(:br) == "<br />"
+    assert Internals.render_self_closing_tag(:br, %{class: "foo"}) == "<br class=\"foo\" />"
+  end
+
 end
