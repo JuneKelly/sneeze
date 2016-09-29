d344d4d48697aa36d7896ae468799721be287d57
Shane Kilkelly <shane@kilkelly.me>
2016-09-29 07:51:25 +0100

    Distinguish between list fallthrough and bare_node

--- lib/sneeze.ex
+++ lib/sneeze.ex
@@ -13,8 +13,10 @@ defmodule Sneeze do
         Internals.render_tag(tag_name)
       [tag_name, attributes] when is_map(attributes) ->
         Internals.render_tag(tag_name, attributes)
-      bare_node ->
+      bare_node when not is_list(bare_node) ->
         to_string bare_node
+      _ ->
+        ""  # TODO: figure out this error case
     end
   end
 
