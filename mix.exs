18c4bd59a3884c1edc8d97f191d1bc884fcf101d
Shane Kilkelly <shane@kilkelly.me>
2018-04-24 21:35:47 +0100

    Update `html_entities` dependency to version 0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sneeze.Mixfile do
 
   def project do
     [app: :sneeze,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      description: "Render Elixir data to HTML. Inspired by Hiccup.",
      package: package(),
@@ -29,7 +29,7 @@ defmodule Sneeze.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:html_entities, "~> 0.3"},
+    [{:html_entities, "~> 0.4"},
      {:ex_doc, ">= 0.14.1", only: :dev}]
   end
 
