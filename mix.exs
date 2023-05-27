d4069d56e1f397a90f32bfe3a4e15fba328942a0
June Kelly <june@cnjctr.com>
2023-05-27 11:05:35 +0100

    update version to 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sneeze.Mixfile do
   def project do
     [
       app: :sneeze,
-      version: "1.2.1",
+      version: "1.3.0",
       elixir: "~> 1.6",
       description: "Render Elixir data to HTML. Inspired by Hiccup.",
       package: package(),
